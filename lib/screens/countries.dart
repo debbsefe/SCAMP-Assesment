5da081bcf382007ebce87102a3393bb5fcbcab20
Mamus Eferha <eferhamamus@gmail.com>
2020-05-09 11:57:13 +0100

    feat: PODO model done

--- lib/screens/countries.dart
+++ lib/screens/countries.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:scamp_assesment/models/model.dart';
+import 'package:http/http.dart';
 
 class Countries extends StatefulWidget {
   @override
@@ -6,10 +9,49 @@ class Countries extends StatefulWidget {
 }
 
 class _CountriesState extends State<Countries> {
+  
+
+List <Summary> countryList = new List();
+  void initState(){
+    _getApi();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: countryList.length, 
+        itemBuilder: (context, index){
+          return ListTile(title: Text(countryList[index].global.toString()),
+          subtitle: Text(countryList[index].countries.toString()),
+          );
+      }
+      ),
     );
   }
+
+  Future<List<Summary>>_getApi() async {
+
+    try{
+      String url = "https://api.covid19api.com/summary";
+
+    Response response = await get(url);
+    final responseBody = response.body;
+    var responseList = json.decode(responseBody) as List;
+    List<Summary>listvalues = responseList.map((e) => Summary.fromJson(e)).toList();
+
+    setState(() {
+      countryList.addAll(listvalues);
+    });
+      return listvalues;
+
+      
+      }catch (e){
+        print(e.toString());
+        return null;
+      }
+
+    }
+    
 }
---
+++ lib/services/api.dart
@@ -0,0 +1,6 @@
+// class Api {
+  
+//   Future getRequest {
+//     return 
+//   }
+// }
